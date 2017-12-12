bbcadddfa4f4640e2c4a2cf92580aee97af959e4
Boris Markov <bomarxbox@hotmail.com>
2017-12-12 17:27:16 +0200

    Added Homework.

--- Arrays/src/examples/SimpleNumLess.java
+++ Arrays/src/examples/SimpleNumLess.java
@@ -1,9 +1,27 @@
 package examples;
 
+import java.util.Scanner;
+
 public class SimpleNumLess {
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void SNL(int n){
+		boolean[] x = new boolean[n+1];
+		for(int i=0;i<=n;i++)x[i]=true;
+		for(int i=2;i<=n;i++){
+			if(x[i]==true){
+				System.out.println(i);
+				for(int j=i+i;j<=n;j+=i){
+					x[j]=false;
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		// For Homework: Use Eratosten's algorithm
+		System.out.print("Enter N: ");
+		int N=Integer.parseInt(sc.nextLine());
+		SNL(N);
 	}
 
 }
